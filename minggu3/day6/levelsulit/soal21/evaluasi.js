1559d887ec158403516fbdda115bcda12e2520e3
Thoriq.Cyber <mohdthoriq24@gmail.com>
2025-09-22 13:24:01 +0700

    evaluasi minggu 3

--- minggu3/day6/levelsulit/soal21/evaluasi.js
+++ minggu3/day6/levelsulit/soal21/evaluasi.js
@@ -1,9 +1,16 @@
-async function data() {
-    let data = await fetch('https://jsonplaceholder.typicode.com/comments')
-    data = await data.json()
-    data.filter((item) => {
-        item.email.includes('@gmail.com')
-        console.log(item)
-    })
+async function getComments() {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/comments")
+    let data = await res.json()
+
+    // Filter email yang mengandung @gmail.com
+    let gmailComments = data.filter(c => c.email.includes("@gmail.com"))
+
+    console.log("Jumlah komentar dengan email Gmail:", gmailComments.length)
+    console.log("Daftar komentar Gmail:", gmailComments)
+  } catch (error) {
+    console.log("Error:", error)
+  }
 }
-data()
+
+getComments()
